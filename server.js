cff39c26fb1b5ca68144ac137131569bc59e91a8
Magdalena Wild <maggieprophete2425@gmail.com>
2023-04-04 13:42:49 -0400

    added post route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path=require("path")
 const fs=require("fs")
 const util=require("util")
 const readFileAsync=util.promisify(fs.readFile)
+const writeFileAsync=util.promisify(fs.writeFile)
 const getnotes=()=>readFileAsync("./db/db.json","utf8").then(data=>[].concat(JSON.parse(data)))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -19,7 +20,20 @@ app.get("/notes",(req,res)=>{
 app.get("/api/notes",(req,res)=>{
     getnotes().then(notes=>{
         res.json(notes)
-    })
+    }).catch(err=>res.json(err))
+})
+app.post("/api/notes",(req,res)=>{
+    getnotes().then(notes=>{
+        const newNote={
+            title:req.body.title,
+            text:req.body.text,
+            id:Math.floor(Math.random()*1000)
+        }
+        const notearray=[...notes,newNote]
+        writeFileAsync("./db/db.json",JSON.stringify(notearray)).then(()=>{
+            res.json({msg:"okay"})
+        })
+    }).catch(err=>res.json(err))
 })
 app.listen(port, function() {
 
